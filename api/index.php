e5809b2f3aae351c5019c7169c30b789b17ffa80
chanjunweimy <chanjunweimy@gmail.com>
2017-10-21 17:19:04 +0800

    php bug
    Signed-off-by: chanjunweimy <chanjunweimy@gmail.com>

--- api/index.php
+++ api/index.php
@@ -1,14 +1,3 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-// get the HTTP method, path and body of the request
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-$input = json_decode(file_get_contents('php://input'),true);
-
-// create SQL based on HTTP method
-switch ($method) {
-  case 'GET':
-    echo '{"message":"helloworld"}'
-}
+ 	echo json_encode('{"message":"done"}')
 ?>
