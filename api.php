b9eb10cf8d3ce3b82585ab01641062e5c67a79ce
chanjunweimy <chanjunweimy@gmail.com>
2017-10-21 16:56:31 +0800

    added api.php
    Signed-off-by: chanjunweimy <chanjunweimy@gmail.com>

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+ 
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$input = json_decode(file_get_contents('php://input'),true);
+
+// create SQL based on HTTP method
+switch ($method) {
+  case 'GET':
+    echo '{"message":"helloworld"}'
+}
+ 
